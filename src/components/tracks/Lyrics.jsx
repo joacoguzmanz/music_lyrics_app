5a98bd167332bcaf52952d1d6f33baa94ac4ec35
joacoguzmanz <joaquingusman77@gmail.com>
2023-01-07 22:30:01 +0100

    added styles to lyrics page

--- src/components/tracks/Lyrics.jsx
+++ src/components/tracks/Lyrics.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import {useState, useEffect, useContext} from "react";
 import {Link, useParams} from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faRecordVinyl, faUser, faTriangleExclamation, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
 import {dataMMContext} from "../../Context";
 
 const Lyrics = () => {
@@ -38,21 +38,39 @@ const Lyrics = () => {
     } else {
         return (
             <>
-                <h1 className='mb-9'>Lyrics of {track.track_name} by {track.artist_name}</h1>
+                <div className='mb-8 mt-4'>
+                    <h1 className='mb-5 text-3xl font-semibold text-center'>{track.track_name}</h1>
+                    <div className='flex justify-around py-'>
+                        <div className=''>
+                            <h3 className='text-sm'><FontAwesomeIcon icon={ faUser } /> Artist</h3>
+                            <p className='font-medium'>{track.artist_name}</p>
+                        </div>
+                        <div className='ml-4'>
+                            <h3 className='text-sm'><FontAwesomeIcon icon={ faRecordVinyl } /> Album</h3>
+                            <p className='font-medium'>{track.album_name}</p>
+                        </div>
+                    </div>
+                </div>
 
-                <p className='mb-9'>{lyrics.lyrics_body}</p>
+                <p className='mb-8 leading-relaxed'>{lyrics.lyrics_body}</p>
 
-                <h3>
+                <h3 className='text-xl mb-1'>Genres</h3>
+                <p className='bg-slate-200 rounded-full py-1 px-4 w-fit'>
                     {typeof track.primary_genres === 'undefined' || track.primary_genres === null
                         ? "No genre"
                         : track.primary_genres.music_genre_list.map(
                             item => item.music_genre.music_genre_name
                         )}
-                </h3>
+                </p>
 
-                <p>Explicit? {track.explicit === 0 ? 'No' : 'Yes'}</p>
+                {
+                    track.explicit === 0
+                    ? <p className="font-medium my-10 text-green-500"><FontAwesomeIcon icon={faThumbsUp} /> Safe for all audiences</p>
+                    : <p className="font-medium my-10 text-red-500"><FontAwesomeIcon icon={faTriangleExclamation} /> Explicit content</p>
+                }
 
-                <Link to='/' className='inline-block py-3 px-5 mt-10 bg-slate-100 rounded-full'>
+
+                <Link to='/' className='inline-block py-3 px-5 bg-slate-100 rounded-full'>
                     <FontAwesomeIcon icon={faArrowLeft} className='mr-2' /> Go back
                 </Link>
             </>
